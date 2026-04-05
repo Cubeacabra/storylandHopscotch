492da4bdc4271852f42ca86bad71721264169046
U-LAPTOP-8E2SPAP7\damad <ethanmcfadden101@gmail.com>
2026-04-05 14:07:58 -0700

    Added functionality for the LED strip

--- main.c
+++ main.c
@@ -3,9 +3,16 @@
 #include <SDL2/SDL.h> //For Sound Library (The core of it)
 #include <SDL2/SDL_mixer.h> //For Sound Library (The actual sound stuff we need)
 #include <signal.h>
+#include "ws2811.h" //For the LED strip
 
 
-//Define the pins we used in terms of wiring pi's language
+//Special Defines for the LED Strip Stuff To Work
+#define TARGET_FREQ WS2811_TARGET_FREQ //Signal Timing (how fast bits are sent)
+#define GPIO_PIN 18 //Needs to be GPIO Pin 18 for this to work ig
+#define DMA 10 //Channel timing idk what means
+#define LED_COUNT 10 //LEDs on the strip, can be changed later
+
+//Define the other pins we used in terms of wiring pi's language
 #define ledStrip 0
 #define laserSensor 1
 
@@ -13,24 +20,44 @@
 volatile int keepRunning = 1;
 
 void intHandler(int dummy) {
-    keepRunning = 0;
+	keepRunning = 0;
 }
 
 
+//This is basically the object we use to control the LED strip itself
+ws2811_t ledstring = {
+	.freq = TARGET_FREQ, 
+	.dmanum = DMA,
+	.channel[0] = {
+		.gpionum = GPIO_PIN, //Which pin sends data
+		.count = LED_COUNT, //How many LEDs
+		.invert = 0,
+		.brightness = 255, //Max Brightness
+	},
+};
+
+
+
 int main(void){
-signal(SIGINT, intHandler);
+	signal(SIGINT, intHandler);
 	// When initialize wiring failed, print message to screen
 	if(wiringPiSetup() == -1){
 		printf("setup wiringPi failed !");
 		return 1;
 	}
 
+	//Initialize LED strip
+	//ws2811_init(&ledstring);
+
+	//Initialize and If LED fails, print message to screen
+	if (ws2811_init(&ledstring) != WS2811_SUCCESS) {
+		printf("LED init failed\n");
+		return 1;
+	}
+
 	//Declare pins as input or output
-	pinMode(ledStrip, OUTPUT);
 	pinMode(laserSensor, INPUT);
-	
-    	//HIGH is off, so set the lights off to start	
-	digitalWrite(ledStrip, HIGH);
+
 
 
 	//Initialize the sound libray itself
@@ -68,35 +95,56 @@ signal(SIGINT, intHandler);
 
 		// Indicate that button has pressed down
 		if(currentLaserState == 0){
-			//Led on
-			digitalWrite(ledStrip, LOW);
-			//play laserSound IFF the lastButton state was off (avoids constant replaying sound and spam and doom)
+			//play cheerSound IFF the lastLaser state was off (avoids constant replaying sound and spam and doom)
 			if (lastLaserState == 1) {
 				//Play cheer sound once
 				Mix_PlayChannel(-1, cheerSound, 0);
-				//Turn LED On
-				digitalWrite(ledStrip, LOW);
+				//Loop over LED strip and turn them all on
+
+				for (int i = 0; i < LED_COUNT; i++) {
+					//LED Strip uses GRB Format Like A Weirdo (0x00GGRRBB)
+					//Make a line of 3 colors
+					if (i % 3 == 0) {
+						ledstring.channel[0].leds[i] = 0x00FFC0CB; //Pink
+					} else if ((i + 1) % 3 == 0) {
+						ledstring.channel[0].leds[i] = 0x00C2B280; //Sandy Yellow
+					} else {
+						ledstring.channel[0].leds[i] = 0x0039FF14; //Neon Green
+
+					}
+				}
+				ws2811_render(&ledstring); //Actually Renders the New Colors, Old Code Just Changed Data in the Object We made at the Start
+
+
+
 
 			}
 			//printf("...LED on\n");
 
 		}
 		else{
+			if (lastLaserState == 0) { //Only set all the lights off if the last state was on, so we not spanning them turning off
 			//Turn LED & Laser Off
-			digitalWrite(ledStrip, HIGH);
+			 for (int i = 0; i < LED_COUNT; i++) {
+                                        ledstring.channel[0].leds[i] = 0x00000000; //Turn All LEDs off
+                                }
+                                ws2811_render(&ledstring); //Actually Renders the New Colors, Old Code Just Changed Data in the Object We made at the Start
 
 			//printf("LED off...\n");
+			}
 		}
 		lastLaserState = currentLaserState;
 
-
+		delay(10); //Add delay or else pi will just go waaaaay too fast, makes it better
 	}
 
 
-	// Clean up sound library stuff before end program
-	Mix_FreeChunk(cheerSound);
-	Mix_CloseAudio();
-	SDL_Quit();
+	// Clean up sound & Lights library stuff before end program
+	Mix_FreeChunk(cheerSound); //Sound Clear
+	Mix_CloseAudio(); //Sound Clear
+	SDL_Quit(); //Sound Clear
+	ws2811_fini(&ledstring); //Lights Clear
+
 
 	//printf("Sound finished!\n");
 
